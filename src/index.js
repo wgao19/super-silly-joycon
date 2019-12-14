63af98b18360abba3c16a9ef027a248d81bfc716
Yishu See <yishuyishu@gmail.com>
2019-12-14 11:39:04 +0800

    Add PasswordUI

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { gamepadConnect, gamepadDisconnect } from "./gamepad";
 // import NintendoUI from "./NintendoUI";
+import PasswordUI from "./PasswordUI";
 
 import "./styles.css";
 
@@ -21,6 +22,7 @@ const JoyConController = () => {
       {/* <NintendoUI /> */}
       <div className="display" id="gamepad-display" />
       <div className="display" id="button-x-display" />
+      <PasswordUI />
     </div>
   );
 };
