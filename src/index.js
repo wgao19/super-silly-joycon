30ec93cedb5a0247cd97805e6490d2dfd21ccd2f
Wei Gao <wgao19@gmail.com>
2019-12-14 11:38:32 +0800

    add test

--- src/index.js
+++ src/index.js
@@ -21,6 +21,9 @@ const JoyConController = () => {
       {/* <NintendoUI /> */}
       <div className="display" id="gamepad-display" />
       <div className="display" id="button-x-display" />
+      {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12].map(idx => (
+        <div className="button-display" data-button={idx} />
+      ))}
     </div>
   );
 };
