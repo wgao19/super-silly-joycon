f637e8e96f81d3924ed0a6e6abdc0d99bc7b03ab
Yishu See <yishuyishu@gmail.com>
2019-12-14 14:35:57 +0800

    Add O emoji

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,14 @@
-export const poseToChar = poses => {
-  return "🙅‍♀️";
+export const poseToChar = positions => {
+  if (bodyPartsAreClose(positions.leftWrist, positions.rightWrist)) {
+    return "🙆";
+  }
+
+  return "🙅";
+};
+
+const bodyPartsAreClose = (a, b) => {
+  const diffX = Math.abs(a.position.x - b.position.x);
+  const diffY = Math.abs(a.position.y - b.position.y);
+
+  return diffX < 10 && diffY < 10;
 };
