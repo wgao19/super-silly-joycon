4b154f75a44a0ddb2a4c3fdee3c6d7ca229da019
Yishu See <yishuyishu@gmail.com>
2019-12-14 17:47:41 +0800

    Add cow gif to EndUI

--- src/EndUI.js
+++ src/EndUI.js
@@ -1,5 +1,10 @@
 import React from "react";
 
 export default function EndUI() {
-  return <h1>end liao! go home!</h1>;
+  return (
+    <>
+      <h1>end liao! go home!</h1>
+      <img src={process.env.PUBLIC_URL + "/images/cow.gif"} />;
+    </>
+  );
 }
